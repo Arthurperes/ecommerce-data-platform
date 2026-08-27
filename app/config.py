e5659fc34016675d6fdf696464c6eef444db3ac5
Arthurperes <arthurhenriquecperesfmu@gmail.com>
2026-08-27 20:39:59 -0300

    Update database and Kafka configuration variables

--- app/config.py
+++ app/config.py
@@ -3,16 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+# Kafka
+KAFKA_BOOTSTRAP_SERVERS = os.getenv(
+    "KAFKA_BOOTSTRAP_SERVERS",
+    "localhost:9092"
+)
 
+TOPIC_ECOMMERCE_EVENTS = os.getenv(
+    "TOPIC_ECOMMERCE_EVENTS",
+    "ecommerce_events_raw"
+)
+
+# PostgreSQL
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", 5432))
-POSTGRES_DB = os.getenv("POSTGRES_DB", "stockdb")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "stockuser")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "stockpass")
-
-TOPIC_STOCK_PRICES = os.getenv("TOPIC_STOCK_PRICES", "stock_prices_raw")
-TOPIC_STOCK_ORDERS = os.getenv("TOPIC_STOCK_ORDERS", "stock_orders_raw")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "ecommercedb")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "ecommerceuser")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "ecommercepass")
 
 POSTGRES_URL = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
